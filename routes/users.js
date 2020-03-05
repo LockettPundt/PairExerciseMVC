e09a8add26f40467a83333ba11e22ca14042bb7e
Lockett Pundt <kalifornium@hotmail.com>
2020-03-05 11:09:55 -0500

    users route for login and signup

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,43 @@ const express = require('express'),
   albumsModel = require('../models/albumsModel'),
   router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+router.get( '/', function(req, res, next) {
+  res.render('template', {
+    locals: {
+      title: 'users'
+    },
+    partials: {
+      partial: 'partial-users'
+    }
+  })
+  //res.send('respond with a resource');
 });
+
+router.get('/signup', async (req, res) => {
+
+  res.render('template', {
+    locals: {
+      title: 'Sign Up'
+    },
+    partials: {
+      partial: 'partial-signup'
+    }
+  })
+});
+
+router.get('/login', async (req, res) => {
+
+  res.render('template', {
+    locals: {
+      title: 'Log in'
+    },
+    partials: {
+      partial: 'partial-login'
+    }
+  })
+});
+
+
 
 module.exports = router;
