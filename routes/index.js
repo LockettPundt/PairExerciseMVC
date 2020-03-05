8a2b2469b0a453af6e3d0c89ee084f65102382dc
Lockett Pundt <kalifornium@hotmail.com>
2020-03-05 11:08:36 -0500

    changed the index to be the landing page.

--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,10 @@ const express = require('express'),
 
 
 router.get('/', async (req, res, next) => {
-  const allAlbums = await albumsModel.getAllAlbums();
-  //console.log(allAlbums);
 
   res.render('template', { 
     locals: {
-      title: 'Albums',
-      allAlbums: allAlbums
+      title: 'Hi there. Welcome.',
     },
     partials: {
       partial: 'partial-index'
@@ -19,29 +16,6 @@ router.get('/', async (req, res, next) => {
     });
 });
 
-router.get('/:id?', async (req, res, next) => {
-  const {id} = req.params;
-  const albumReviews = await albumsModel.getAllReviews(id);
-  console.log(albumReviews);
-  res.render('template', { 
-    locals: {
-      title: 'Reviews',
-      albumReviews: albumReviews
-    },
-    partials: {
-      partial: 'partial-review'
-    }
-    });
-});
-
-router.post('/', async (req, res) => {
-
-  const {album_id, review_text, review_title, user_name, stars} = req.body;
-  console.log(req.body);
-  const postData = await albumsModel.createReview(album_id, review_title, review_text, stars);
-  console.log(postData);
-  res.send(200);
-})
 
 
 module.exports = router;
