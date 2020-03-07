8d64819e838419396b8b276459f3c7d3b7d8f5d8
Lockett Pundt <kalifornium@hotmail.com>
2020-03-07 07:34:20 -0500

    add an if/else statement to the user login in method to check if the hash is correct and if so, return the boolean.

--- models/userModel.js
+++ models/userModel.js
@@ -19,7 +19,7 @@ class UserModel {
     async addUser() {
         try {
             const response = await db.one(`INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING id`, [this.name, this.email, this.password]);
-            console.log('adding user', this); //can also just type 'this'
+            console.log('adding user', this.name); //can also just type 'this'
             return response;
         } catch (error) {
             return error;
@@ -31,8 +31,8 @@ class UserModel {
             const response = await db.one(`SELECT id, name, password FROM users WHERE email = $1`, [this.email]);
             console.log('logging in user', response.name, this.email);
             const isValid = this.checkPassword(response.password);
-            !!isValid ? console.log('Success!') :console.log('NOPE!!!!!');
-            
+            const {id, name} = response;
+            return !!isValid ? {isValid, id, name} : isValid;
         } catch (error) {
             return error;
         }
